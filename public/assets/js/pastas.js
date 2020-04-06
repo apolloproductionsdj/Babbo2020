5ef24aeea8bbf2ffca08564ea4deae2bcbfb35d9
Mike Torres <apolloproductionsdj@gmail.com>
2020-04-06 21:22:45 +0000

    added modal for what the kale

--- public/assets/js/pastas.js
+++ public/assets/js/pastas.js
@@ -13,8 +13,27 @@ document.getElementById('button-francaise').addEventListener('click',
     document.querySelector('.bg-modal-francaise').style.display = 'flex';
   });
 
-document.querySelector('.close').addEventListener('click', 
+document.querySelector('.close-fran').addEventListener('click', 
   function() {
     document.querySelector('.bg-modal-francaise').style.display = 'none';
   });
 
+document.getElementById('button-super').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-super-food-pasta').style.display = 'flex';
+  });
+
+document.querySelector('.close-super').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-super-food-pasta').style.display = 'none';
+  });
+
+document.getElementById('button-kale').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-kale').style.display = "flex";
+  });
+
+document.querySelector('.close-kale').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-kale').style.display = 'none';
+  });
