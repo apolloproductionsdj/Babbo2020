72a3e05c512a821c7389b563c2a95b6833713a7e
Mike Torres <apolloproductionsdj@gmail.com>
2020-04-10 23:40:17 +0000

    finished first 7 pastas modal

--- public/assets/js/pastas.js
+++ public/assets/js/pastas.js
@@ -28,6 +28,16 @@ document.querySelector('.close-fran').addEventListener('click',
     document.querySelector('.bg-modal-francaise').style.display = 'none';
   });
 
+ $(document).ready(function () {
+        var small={width: "200px",height: "116px"};
+        var large={width: "400px",height: "232px"};
+        var count=1; 
+        $("#img-francaise").css(small).on('click',function () { 
+            $(this).animate((count==1)?large:small);
+            count = 1-count;
+        });
+    });
+
 document.getElementById('button-super').addEventListener('click', 
   function() {
     document.querySelector('.bg-modal-super-food-pasta').style.display = 'flex';
@@ -37,6 +47,16 @@ document.querySelector('.close-super').addEventListener('click',
   function() {
     document.querySelector('.bg-modal-super-food-pasta').style.display = 'none';
   });
+
+ $(document).ready(function () {
+        var small={width: "200px",height: "116px"};
+        var large={width: "400px",height: "232px"};
+        var count=1; 
+        $("#img-super").css(small).on('click',function () { 
+            $(this).animate((count==1)?large:small);
+            count = 1-count;
+        });
+    });
 
 document.getElementById('button-kale').addEventListener('click', 
   function() {
@@ -48,6 +68,16 @@ document.querySelector('.close-kale').addEventListener('click',
     document.querySelector('.bg-modal-kale').style.display = 'none';
   });
 
+$(document).ready(function () {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-kale").css(small).on('click', function () {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
 document.getElementById('button-pomodoro').addEventListener('click', 
   function() {
     document.querySelector('.bg-modal-pomodoro').style.display = "flex";
@@ -58,5 +88,52 @@ document.querySelector('.close-pomodoro').addEventListener('click',
     document.querySelector('.bg-modal-pomodoro').style.display = 'none';
   });
 
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-pomodoro").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
 
+document.getElementById('button-supremo').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-supremo').style.display = "flex";
+  });
+
+document.querySelector('.close-supremo').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-supremo').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-supremo").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
       
+document.getElementById('button-primavera').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-primavera').style.display = "flex";
+  });
+
+document.querySelector('.close-primavera').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-primavera').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-primavera").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
