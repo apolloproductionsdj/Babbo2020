4bc053ea53b27344db6b0abce76bf3cafd7e8388
Mike Torres <apolloproductionsdj@gmail.com>
2020-04-10 22:17:28 +0000

    allowed for image to grow when pressed

--- public/assets/js/pastas.js
+++ public/assets/js/pastas.js
@@ -8,6 +8,16 @@ document.querySelector('.close-zoo').addEventListener('click',
     document.querySelector('.bg-modal').style.display = 'none';
   });
 
+ $(document).ready(function () {
+        var small={width: "200px",height: "116px"};
+        var large={width: "400px",height: "232px"};
+        var count=1; 
+        $("#imgtab").css(small).on('click',function () { 
+            $(this).animate((count==1)?large:small);
+            count = 1-count;
+        });
+    });
+
 document.getElementById('button-francaise').addEventListener('click', 
   function() {
     document.querySelector('.bg-modal-francaise').style.display = 'flex';
@@ -38,6 +48,15 @@ document.querySelector('.close-kale').addEventListener('click',
     document.querySelector('.bg-modal-kale').style.display = 'none';
   });
 
+document.getElementById('button-pomodoro').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-pomodoro').style.display = "flex";
+  });
+
+document.querySelector('.close-pomodoro').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-pomodoro').style.display = 'none';
+  });
 
 
       
