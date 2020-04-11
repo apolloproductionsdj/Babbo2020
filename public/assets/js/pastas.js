a49a4dca72bf8573582f8c148d346cfe8c15b656
Mike Torres <apolloproductionsdj@gmail.com>
2020-04-11 20:09:08 +0000

    finished pastas menu with images and modals

--- public/assets/js/pastas.js
+++ public/assets/js/pastas.js
@@ -137,3 +137,441 @@ $(document).ready(function() {
     count = 1 - count;
   });
 });
+document.getElementById('button-creamy').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-creamy').style.display = "flex";
+  });
+
+document.querySelector('.close-creamy').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-creamy').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-creamy").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+document.getElementById('button-rustico').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-rustico').style.display = "flex";
+  });
+
+document.querySelector('.close-rustico').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-rustico').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-rustico").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-valducci').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-valducci').style.display = "flex";
+  });
+
+document.querySelector('.close-valducci').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-valducci').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-valducci").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-bolognese').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-bolognese').style.display = "flex";
+  });
+
+document.querySelector('.close-bolognese').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-bolognese').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-bolognese").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-puglia').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-puglia').style.display = "flex";
+  });
+
+document.querySelector('.close-puglia').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-puglia').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-puglia").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-limone').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-limone').style.display = "flex";
+  });
+
+document.querySelector('.close-limone').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-limone').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-limone").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-italian').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-italian').style.display = "flex";
+  });
+
+document.querySelector('.close-italian').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-italian').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-italian").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-spaghetti').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-spaghetti').style.display = "flex";
+  });
+
+document.querySelector('.close-spaghetti').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-spaghetti').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-spaghetti").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-chipotle').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-chipotle').style.display = "flex";
+  });
+
+document.querySelector('.close-chipotle').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-chipotle').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-chipotle").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-garlic').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-garlic').style.display = "flex";
+  });
+
+document.querySelector('.close-garlic').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-garlic').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-garlic").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-martini').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-martini').style.display = "flex";
+  });
+
+document.querySelector('.close-martini').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-martini').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-martini").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-spicy').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-spicy').style.display = "flex";
+  });
+
+document.querySelector('.close-spicy').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-spicy').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-spicy").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-parmesean').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-parmesean').style.display = "flex";
+  });
+
+document.querySelector('.close-parmesean').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-parmesean').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-parmesean").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-pesto').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-pesto').style.display = "flex";
+  });
+
+document.querySelector('.close-pesto').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-pesto').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-pesto").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-roasted').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-roasted').style.display = "flex";
+  });
+
+document.querySelector('.close-roasted').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-roasted').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-roasted").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-stuffed').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-stuffed').style.display = "flex";
+  });
+
+document.querySelector('.close-stuffed').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-stuffed').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-stuffed").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-fettuccine').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-fettuccine').style.display = "flex";
+  });
+
+document.querySelector('.close-fettuccine').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-fettuccine').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-fettuccine").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-three').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-three').style.display = "flex";
+  });
+
+document.querySelector('.close-three').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-three').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-three").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-cajun').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-cajun').style.display = "flex";
+  });
+
+document.querySelector('.close-cajun').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-cajun').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-cajun").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-shrimp').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-shrimp').style.display = "flex";
+  });
+
+document.querySelector('.close-shrimp').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-shrimp').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-shrimp").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-great').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-great').style.display = "flex";
+  });
+
+document.querySelector('.close-great').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-great').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-great").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
+
+document.getElementById('button-lasagna').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-lasagna').style.display = "flex";
+  });
+
+document.querySelector('.close-lasagna').addEventListener('click', 
+  function() {
+    document.querySelector('.bg-modal-lasagna').style.display = 'none';
+  });
+
+$(document).ready(function() {
+  var small = {width: "200px", height: "116px"};
+  var large = {width: "400px", height: "232px"};
+  var count = 1;
+  $("#img-lasagna").css(small).on('click', function() {
+    $(this).animate((count==1) ? large : small);
+    count = 1 - count;
+  });
+});
