78364a2dbee0bb338e8849b726ecfc57cd797329
Mike Torres <apolloproductionsdj@gmail.com>
2020-04-16 03:53:34 +0000

    add to cart button in modal

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,11 +1,48 @@
-var removeCartItemsButtons = document.getElementsByClassName('btn-danger');
-
-console.log(removeCartItemsButtons);
-
-for (var i = 0; i < removeCartItemsButtons.length; i++) {
-  var button = removeCartItemsButtons[i]
-  button.addEventListener('click', function(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove();
-  })
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
 }
+function ready() {
+  var removeCartItemsButtons = document.getElementsByClassName('btn-danger');
+  console.log(removeCartItemsButtons);
+  for (var i = 0; i < removeCartItemsButtons.length; i++) {
+    var button = removeCartItemsButtons[i]
+    button.addEventListener('click', removeCartItem);
+  }
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (var i = 0; i < quantityInputs.length; i++) {
+      var input = quantityInputs[i];
+      input.addEventListener('change', quantityChanged);
+  }
+}
+
+function removeCartItem() {
+  var buttonClicked = event.target
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal()
+}
+
+function quantityChanged(event) {
+  var input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1
+  }
+  updateCartTotal()
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+function updateCartTotal () {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i]
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+    var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+    var price = parseFloat(priceElement.innerText.replace('$', ''))
+    var quantity = quantityElement.value
+    total = total + (price * quantity);
+  }
+  total = Math.round(total * 100) / 100; 
+  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+} 
